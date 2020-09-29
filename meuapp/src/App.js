15ec0083e08dbb0293e033c85d287439f68dc4cd
Benhur <benhur_zambiasi@hotmail.com>
2020-09-29 14:48:39 -0300

    Exercício 05

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -3,13 +3,18 @@ import React from 'react'
 // import Exerc01 from './Exerc_01';
 // import Exerc02 from './Exerc_02';
 //import Exerc03 from './Exerc03/Exerc_03';
-import Exerc04 from './Exerc04/Exerc_04';
+//import Exerc04 from './Exerc04/Exerc_04';
+import Exerc05 from './Exerc05/Exerc_05';
+import Teste from './teste/App';
 
 
 
 const App = () => {
 
-  return <Exerc04 />
+  //return <Teste />
+  return (
+    <>
+      <Exerc05 />
+    </>)
 }
 export default App;
- 
