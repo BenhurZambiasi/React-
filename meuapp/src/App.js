27d5e2d477373220a22ef967d0b7f9a987d2d180
Benhur <benhur_zambiasi@hotmail.com>
2020-09-30 14:09:54 -0300

    Form input

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,49 +1,53 @@
 
 import React from 'react'
-import UseRef from './UseRef';
-import UserContext from './UserContext/UserContext';
-import Exerc01 from './Exerc_01';
-import Exerc02 from './Exerc_02';
-import Exerc03 from './Exerc03/Exerc_03';
-import Exerc04 from './Exerc04/Exerc_04';
-import Exerc05 from './Exerc05/Exerc_05';
-import Exerc06 from './Exerc06/Exerc_06';
-
-
-
+// import UseRef from './UseRef';
+// import UserContext from './UserContext/UserContext';
+// import Exerc01 from './Exerc_01';
+// import Exerc02 from './Exerc_02';
+// import Exerc03 from './Exerc03/Exerc_03';
+// import Exerc04 from './Exerc04/Exerc_04';
+// import Exerc05 from './Exerc05/Exerc_05';
+// import Exerc06 from './Exerc06/Exerc_06';
+import Form from './Form';
 
 const App = () => {
 
-
-  return (
-    <>
-      <h1>UseContext</h1>
-      <UserContext />
-      <hr />
-      <h1>UseRef</h1>
-      <UseRef />
-      <hr />
-      <h1>Exercício 01</h1>
-      <Exerc01 />
-      <hr />
-      <h1>Exercício 02</h1>
-      <Exerc02 />
-      <hr />
-      <h1>Exercício 03</h1>
-      <Exerc03 />
-      <hr />
-      <h1>Exercício 04</h1>
-      <Exerc04 />
-      <hr />
-      <h1>Exercício 05</h1>
-      <Exerc05 />
-      <hr />
-      <h1>Exercício 06</h1>
-      <Exerc06 />
-
-
-
-    </>)
-
+  return <Form />
 }
+
+
+// const App = () => {
+
+
+//   return (
+//     <>
+//       <h1>UseContext</h1>
+//       <UserContext />
+//       <hr />
+//       <h1>UseRef</h1>
+//       <UseRef />
+//       <hr />
+//       <h1>Exercício 01</h1>
+//       <Exerc01 />
+//       <hr />
+//       <h1>Exercício 02</h1>
+//       <Exerc02 />
+//       <hr />
+//       <h1>Exercício 03</h1>
+//       <Exerc03 />
+//       <hr />
+//       <h1>Exercício 04</h1>
+//       <Exerc04 />
+//       <hr />
+//       <h1>Exercício 05</h1>
+//       <Exerc05 />
+//       <hr />
+//       <h1>Exercício 06</h1>
+//       <Exerc06 />
+
+
+
+//     </>)
+
+// }
 export default App;
---
+++ meuapp/src/Form.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Form = () => {
+  const [nome, setNome] = React.useState(' ');
+  const [email, setEmail] = React.useState(' ');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  const [form, setForm] = React.useState({
+    nome: '',
+    email: '',
+  })
+
+  function handleChange({ target }) {
+    const { id, value } = target;
+    setForm({ ...form, [id]: value })
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="nome">Nome</label>
+        <input id="nome" name="nome" value={nome} type="text" onChange={(event) => setNome(event.target.value)} />
+
+        <label htmlFor="email">Email</label>
+        <input id="email" name="email" value={email} type="email" onChange={(event) => setEmail(event.target.value)} />
+        <button>Enviar</button>
+      </form>
+
+      <hr />
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="nome">Nome</label>
+        <input id="nome" name="nome" value={form.nome} type="text" onChange={handleChange} />
+
+        <label htmlFor="email">Email</label>
+        <input id="email" name="email" value={form.email} type="email" onChange={handleChange} />
+        <button>Enviar</button>
+      </form>
+    </div>
+
+  );
+};
+
+export default Form;
