50bbd7a124af486eefecb4e58f8c5c3736bc0b02
Benhur <benhur_zambiasi@hotmail.com>
2020-10-01 14:24:16 -0300

    Novo projeto

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -12,44 +12,47 @@ import Form from './Form';
 import Exerc07 from './Exerc07/Exerc_07';
 import Desafio from './DesafioForm/Desafio';
 
+
 const App = () => {
 
-  return <Desafio />
+
+  //   return
+  // }
+
+  return (
+    <>
+      <h1>Desafio Form</h1>
+      <Desafio />
+      <hr />
+      <h1>Form imput</h1>
+      <Form />
+      <hr />
+      <h1>UseContext</h1>
+      <UserContext />
+      <hr />
+      <h1>UseRef</h1>
+      <UseRef />
+      <hr />
+      <h1>Exercício 01</h1>
+      <Exerc01 />
+      <hr />
+      <h1>Exercício 02</h1>
+      <Exerc02 />
+      <hr />
+      <h1>Exercício 03</h1>
+      <Exerc03 />
+      <hr />
+      <h1>Exercício 04</h1>
+      <Exerc04 />
+      <hr />
+      <h1>Exercício 05</h1>
+      <Exerc05 />
+      <hr />
+      <h1>Exercício 06</h1>
+      <Exerc06 />
+      <h1>Exercício 07</h1>
+      <Exerc07 />
+    </>)
+
 }
-
-
-// const App = () => {
-
-
-//   return (
-//     <>
-//       <h1>Form imput</h1>
-//       <Form />
-//       <hr />
-//       <h1>UseContext</h1>
-//       <UserContext />
-//       <hr />
-//       <h1>UseRef</h1>
-//       <UseRef />
-//       <hr />
-//       <h1>Exercício 01</h1>
-//       <Exerc01 />
-//       <hr />
-//       <h1>Exercício 02</h1>
-//       <Exerc02 />
-//       <hr />
-//       <h1>Exercício 03</h1>
-//       <Exerc03 />
-//       <hr />
-//       <h1>Exercício 04</h1>
-//       <Exerc04 />
-//       <hr />
-//       <h1>Exercício 05</h1>
-//       <Exerc05 />
-//       <hr />
-//       <h1>Exercício 06</h1>
-//       <Exerc06 />
-//     </>)
-
-// }
 export default App;
